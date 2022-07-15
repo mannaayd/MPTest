847bea672482ef7ca5e294aec8e0c7b72a3eda6b
Aydar Mannanov <aydar.mannanov@victoriavr.com>
2022-07-15 12:06:27 +0200

    init

---
+++ enc_temp_folder/e28e2b427bd976dce6fe8ee090af4c/MPTestGameMode.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MPTestGameMode.h"
+#include "MPTestCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMPTestGameMode::AMPTestGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		UE_LOG(LogTemp, Log, TEXT("hello"));
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
